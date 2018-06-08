e014bf18d02958fe187402165d885a12ee384a1d
Ardhi Lukianto <ardhi@lukianto.com>
2018-06-08 21:18:16 +0700

    reg update

---
+++ config.js
@@ -0,0 +1,29 @@
+'use strict'
+
+module.exports = function(cuk) {
+  const { _, globby, helper, path, fs } = cuk.lib
+
+  let common = {
+    fluidContainer: false,
+    useCdn: true,
+    navbar: "fixed-top navbar-dark bg-primary"
+  }
+
+  return new Promise((resolve, reject) => {
+    let dir = path.join(cuk.pkg.bootswatch.dir, 'node_modules', 'bootswatch', 'dist')
+    if (!fs.existsSync(dir))
+      dir = path.join(cuk.dir.root, 'node_modules', 'bootswatch', 'dist')
+    let dirs = _.map(globby.sync(dir + '/*', {
+      onlyDirectories: true
+    }), d => {
+      return path.basename(d)
+    })
+
+    common.defaultTheme = 'minty'
+    common.themes = dirs
+    resolve({
+      common: common
+    })
+
+  })
+}
